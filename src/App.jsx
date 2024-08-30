efc8b7ba51ace1a87740f7def7abd4cf51bfef22
Xuân Hậu Nguyễn <nguyenxuanhau2003@gmail.com>
2024-08-30 09:38:42 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux'
+import CartContainer from './components/CartContainer'
+import Navbar from './components/Navbar'
+import { useEffect } from 'react'
+import { calculateTotals } from './features/cart/cartSlice'
+import Modal from './components/Modal'
+
+function App() {
+  const { cartItems } = useSelector((state) => state.cart)
+  const { isOpen } = useSelector((state) => state.modal)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(calculateTotals())
+  }, [cartItems])
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
+}
+export default App
